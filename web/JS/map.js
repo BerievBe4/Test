502dfbba835710492ccbcb044729e44ed9464ebd
Daniil Uskov <uskov.dan@mail.ru>
2020-02-07 16:14:31 +0300

    6-th, second day

--- web/JS/map.js
+++ web/JS/map.js
@@ -34,26 +34,39 @@ function init () {
     });
 
     var myPolyline = new ymaps.Polyline([
-        // Указываем координаты вершин ломаной.
+        // Указываем координаты вершин.
         [55.80, 37.50],
         [55.80, 37.40],
         [55.70, 37.50],
         [55.70, 37.40]
-    ], {
-        // Описываем свойства геообъекта.
-        // Содержимое балуна.
-        balloonContent: "Ломаная линия"
-    }, {
+    ], {}, {
         // Задаем опции геообъекта.
-        // Отключаем кнопку закрытия балуна.
-        balloonCloseButton: false,
-        // Цвет линии.
-        strokeColor: "#000000",
-        // Ширина линии.
+        // Цвет с прозрачностью.
+        strokeColor: "#00000088",
+        // Ширину линии.
         strokeWidth: 4,
-        // Коэффициент прозрачности.
-        strokeOpacity: 0.5,
-        draggable: true
+        // Максимально допустимое количество вершин в ломаной.
+        editorMaxPoints: 6,
+        // Добавляем в контекстное меню новый пункт, позволяющий удалить ломаную.
+        editorMenuManager: function (items) {
+            items.push({
+                title: "Удалить линию",
+                onClick: function () {
+                    myMap.geoObjects.remove(myPolyline);
+                }
+            });
+            return items;
+        }
+    });
+
+    // Добавляем линию на карту.
+    myMap.geoObjects.add(myPolyline);
+
+    // Включаем режим редактирования.
+    myPolyline.editor.startEditing();
+
+    var newElem = new ymaps.GeoObjectCollection(null, {
+        preset: 'islands#yellowIcon'
     });
 
     myMap.events.add('click', function (e) {
@@ -77,26 +90,66 @@ function init () {
             // Метку можно перемещать.
             draggable: true
         });
+        newElem.add(newGeoObject);
         myMap.geoObjects
             .add(newGeoObject);
 
     });
 
-    // Обработка события, возникающего при щелчке
-    // правой кнопки мыши в любой точке карты.
-    // При возникновении такого события покажем всплывающую подсказку
-    // в точке щелчка.
-    myMap.events.add('contextmenu', function (e) {
-        myMap.hint.open(e.get('coords'), 'Кто-то щелкнул правой кнопкой');
+    myMap.geoObjects.events.add('dblclick', function (e){
+        var target = e.get('target');
+        myMap.geoObjects.remove(target);
     });
 
-    // Скрываем хинт при открытии балуна.
-    myMap.events.add('balloonopen', function (e) {
-        myMap.hint.close();
-    });
+    myMap.behaviors
+        .disable(['dblClickZoom'])
 
     myMap.geoObjects
-        .add(myGeoObject)
-        .add(myPolyline);
+        .add(myGeoObject);
 
+    var paintProcess;
+
+    // Опции многоугольника или линии.
+    var styles = [
+        {strokeColor: '#ff00ff', strokeOpacity: 0.7, strokeWidth: 3, fillColor: '#ff00ff', fillOpacity: 0.4},
+        {strokeColor: '#ff0000', strokeOpacity: 0.6, strokeWidth: 6, fillColor: '#ff0000', fillOpacity: 0.3},
+        {strokeColor: '#00ff00', strokeOpacity: 0.5, strokeWidth: 3, fillColor: '#00ff00', fillOpacity: 0.2},
+        {strokeColor: '#0000ff', strokeOpacity: 0.8, strokeWidth: 5, fillColor: '#0000ff', fillOpacity: 0.5},
+        {strokeColor: '#000000', strokeOpacity: 0.6, strokeWidth: 8, fillColor: '#000000', fillOpacity: 0.3},
+    ];
+
+    var currentIndex = 0;
+
+    // Создадим кнопку для выбора типа рисуемого контура.
+    var button = new ymaps.control.Button({data: {content: 'Polygon / Polyline'}, options: {maxWidth: 150}});
+    myMap.controls.add(button);
+
+    // Подпишемся на событие нажатия кнопки мыши.
+    myMap.events.add('mousedown', function (e) {
+        // Если кнопка мыши была нажата с зажатой клавишей "alt", то начинаем рисование контура.
+        if (e.get('altKey')) {
+            if (currentIndex == styles.length - 1) {
+                currentIndex = 0;
+            } else {
+                currentIndex += 1;
+            }
+            paintProcess = ymaps.ext.paintOnMap(myMap, e, {style: styles[currentIndex]});
+        }
+    });
+
+    // Подпишемся на событие отпускания кнопки мыши.
+    myMap.events.add('mouseup', function (e) {
+        if (paintProcess) {
+
+            // Получаем координаты отрисованного контура.
+            var coordinates = paintProcess.finishPaintingAt(e);
+            paintProcess = null;
+            // В зависимости от состояния кнопки добавляем на карту многоугольник или линию с полученными координатами.
+            var geoObject = button.isSelected() ?
+                new ymaps.Polyline(coordinates, {}, styles[currentIndex]) :
+                new ymaps.Polygon([coordinates], {}, styles[currentIndex]);
+
+            myMap.geoObjects.add(geoObject);
+        }
+    });
 }
